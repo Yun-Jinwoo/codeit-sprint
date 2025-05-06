49b21cabe19ad0e2aecd6847a92d33de6e43068b
Yun-Jinwoo <yjw3480@naver.com>
2025-05-07 00:12:33 +0900

    ✨ feat: CalendarBody 컴포넌트 구현

---
+++ study/calendar-project/src/Calendar/CalendarBody.jsx
@@ -0,0 +1,64 @@
+import { useContext } from "react";
+import CalendarContext from "./CalendarContext";
+
+const CalendarBody = () => {
+  const { selectedDate, currentMonth, setCurrentMonth, onChange } =
+    useContext(CalendarContext);
+
+  const firstDay = new Date(
+    currentMonth.getFullYear(),
+    currentMonth.getMonth(),
+    1
+  ).getDay();
+
+  const firstDate = new Date(
+    currentMonth.getFullYear(),
+    currentMonth.getMonth(),
+    1
+  );
+  firstDate.setDate(firstDate.getDate() - firstDay);
+
+  const dayList = ["일", "월", "화", "수", "목", "금", "토"];
+  const dateList = [];
+  for (let i = 0; i < 42; i++) {
+    const date = new Date(firstDate);
+    date.setDate(firstDate.getDate() + i);
+    dateList.push(date);
+  }
+
+  return (
+    <div className="calendar-body">
+      <div className="days">
+        {dayList.map((day, index) => (
+          <div key={index} className="day">
+            {day}
+          </div>
+        ))}
+      </div>
+      <div className="dates">
+        {dateList.map((date, index) => (
+          <div
+            key={index + 7}
+            onClick={() => {
+              setCurrentMonth(date);
+              onChange(currentMonth.getMonth(), date);
+            }}
+            className={`date ${
+              date.getMonth() !== currentMonth.getMonth() ? "gray" : ""
+            } ${
+              date.getFullYear() === selectedDate.getFullYear() &&
+              date.getMonth() === selectedDate.getMonth() &&
+              date.getDate() === selectedDate.getDate()
+                ? "selected"
+                : ""
+            }`}
+          >
+            {date.getDate()}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CalendarBody;
