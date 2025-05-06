ed37caeabf5552ae0c18fabceb3439f5e3480579
Yun-Jinwoo <yjw3480@naver.com>
2025-05-06 21:39:06 +0900

    ✨ feat: App.jsx에 6개의 Accordion 작성

--- study/accordion-project/src/App.jsx
+++ study/accordion-project/src/App.jsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import Accordion from "./Accordion/Accordion";
 
 function App() {
-  return <></>;
+  return (
+    <Accordion>
+      <Accordion.Item value="a">
+        <Accordion.Trigger>Title-1</Accordion.Trigger>
+        <Accordion.Pannel>Content-1</Accordion.Pannel>
+      </Accordion.Item>
+      <Accordion.Item value="b">
+        <Accordion.Trigger>Title-2</Accordion.Trigger>
+        <Accordion.Pannel>Content-2</Accordion.Pannel>
+      </Accordion.Item>
+      <Accordion.Item value="c">
+        <Accordion.Trigger>Title-3</Accordion.Trigger>
+        <Accordion.Pannel>Content-3</Accordion.Pannel>
+      </Accordion.Item>
+      <Accordion.Item value="d">
+        <Accordion.Trigger>Title-4</Accordion.Trigger>
+        <Accordion.Pannel>Content-4</Accordion.Pannel>
+      </Accordion.Item>
+      <Accordion.Item value="e">
+        <Accordion.Trigger>Title-5</Accordion.Trigger>
+        <Accordion.Pannel>Content-5</Accordion.Pannel>
+      </Accordion.Item>
+      <Accordion.Item value="f">
+        <Accordion.Trigger>Title-6</Accordion.Trigger>
+        <Accordion.Pannel>Content-6</Accordion.Pannel>
+      </Accordion.Item>
+    </Accordion>
+  );
 }
 
 export default App;
