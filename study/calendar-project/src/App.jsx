c273f7ccd82523b6fcbad5b193e451bdf450e824
Yun-Jinwoo <yjw3480@naver.com>
2025-05-07 00:13:11 +0900

    ✨ feat: App.jsx에 Calendar 컴포넌트 작성

---
+++ study/calendar-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Calendar from "./Calendar/Calendar";
+import "./App.css";
+
+function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleSelectDate = (month, date) => {
+    console.log("selectedDate ::", date);
+    const newDate = new Date(selectedDate);
+    newDate.setMonth(month);
+    newDate.setDate(date);
+    setSelectedDate(date);
+  };
+
+  return (
+    <Calendar onChange={handleSelectDate} value={selectedDate}>
+      <Calendar.Header />
+      <Calendar.Navigator />
+      <Calendar.Body />
+    </Calendar>
+  );
+}
+
+export default App;
